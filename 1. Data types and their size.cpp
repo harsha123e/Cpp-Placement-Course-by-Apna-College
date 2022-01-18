c02191187dc305e588a3d3bb1e86581f576bd3cb
harsha <nsriharsha3@gmail.com>
2022-01-18 11:15:14 +0530

    Create 1. Data types and their size.cpp

---
+++ 1. Data types and their size.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int a;
+    cout << "size of int : " << sizeof(a) << endl;
+    
+    float b;
+    cout << "size of float : " << sizeof(b) << endl;
+    
+    char c;
+    cout << "size of character : " << sizeof(c) << endl;
+    
+    bool d;
+    cout << "size of boolean : " << sizeof(d) << endl;
+    
+    return 0;
+}
