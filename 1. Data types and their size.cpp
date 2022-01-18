16f97a7d61d076dc8f080fdb7794e711dc3cb3b1
harsha <nsriharsha3@gmail.com>
2022-01-18 11:21:46 +0530

    Update 1. Data types and their size.cpp

--- 1. Data types and their size.cpp
+++ 1. Data types and their size.cpp
@@ -16,5 +16,14 @@ int main() {
     bool d;
     cout << "size of boolean : " << sizeof(d) << endl;
     
+    short int si;
+    cout << "size of short int : " << sizeof(si) << endl;
+    
+    long int li;
+    cout << "size of long int : " << sizeof(li) << endl;
+    
+    unsigned int ui;
+    cout << "size of unsigned int : " << sizeof(ui) << endl;
+    
     return 0;
 }
