d5e130232d66e421db445abaf99af3531e598224
harsha <nsriharsha3@gmail.com>
2022-01-18 12:16:59 +0530

    Create 6. terminate on negative input ( while loop ).cpp

---
+++ 6. terminate on negative input ( while loop ).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int n,sum=0;
+    cin >> n;
+    while(n>0){
+        cout<<n<<endl;
+        cin>>n;
+    }
+        
+    return 0;
+}
