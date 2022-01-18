1d2b471a2369dd15ade1ed7897b850949569e8df
harsha <nsriharsha3@gmail.com>
2022-01-18 11:49:38 +0530

    Create input output.cpp

---
+++ input output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int amount1,amount2;
+    
+    cout << "Enter two numbers : ";
+    cin >> amount1 >> amount2;
+    
+    cout << "sum : " << amount1+amount2;
+    
+    return 0;
+}
