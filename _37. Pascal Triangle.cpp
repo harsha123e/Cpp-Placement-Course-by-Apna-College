c4c2d9290d79f18bbcece81100a66dc04fc44e83
harsha <nsriharsha3@gmail.com>
2022-01-21 14:55:34 +0530

    Create _37. Pascal Triangle.cpp

---
+++ _37. Pascal Triangle.cpp
@@ -0,0 +1,24 @@
+//Pascal Triangle
+
+#include <iostream>
+
+using namespace std;
+
+int fact(int n){
+    return n<=1?1:n*fact(n-1);
+}
+
+int main() {
+    
+    int n;
+    cin>>n;
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<fact(i)/(fact(j)*fact(i-j))<<" ";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
